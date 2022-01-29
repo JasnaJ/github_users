f412c7bb1b47300b8285ad72bf164e7a0c99e3e4
JasnaJ <jovanic_jasna@yahoo.com>
2022-01-29 16:01:59 +0100

    search bar

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,23 +1,57 @@
 import React, { useState, useEffect } from "react";
-
+import {Form, Card, Image, Icon} from 'semantic-ui-react'
 
 function Main() {
-	//const [username, setUsername] = useState("");
-	//const [userData, setUserData] = useState(Object);
-    let id=0
-	let URL_users = "https://api.github.com/users"
-    let URL_userId = `https://api.github.com/users/${id}`
+	const [userName, setUserName] = useState("");
+	const [userData, setUserData] = useState("");
+    const [followers, setFollowers] = useState("");
+	const [following, setFolowwing] = useState("");
+	const [repos, setRepos] = useState("");
+    const [avatar, setAvatar] = useState("");
+    const [company, setCompany] = useState("");
+    const [location, setlocation] = useState("");
+	const [email, setEmail] = useState("");
+	const [twitterUsername, setTwitterUsername] = useState("");
+    const [input, setInput] = useState("");
+    const [error, setError] = useState("");
 
-	useEffect(() => {
-		fetch(URL_users)
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data);
-            });
-	},[]);
+	let URL_user = `https://api.github.com/users/${input}`
+
+    const handleSubmit = () =>{
+        fetch(URL_user)
+        .then(res => res.json())
+        .then(data =>{
+            if(data.message){
+                setError(data.message)
+            }
+            else{
+                setUserData(data)
+                console.log(data)
+                setError(null)
+                setInput("")
+            }        
+        });
+    }
+
+    const handleSearch = (e) =>{
+        setInput(e.target.value)
+    }
 
 	return (
 		<div>
+            <div className='navbar'>
+                <div>
+                    <img src='./GitHub-Mark-120px-plus.png' ></img>
+                </div>
+                <div>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Input placeholder="Search Github users" onChange={handleSearch}></Form.Input>
+                        <Form.Button content="Search"></Form.Button>
+                    </Form.Group>
+                </Form>
+                </div>
+            </div>
 		</div>
 	);
 }
